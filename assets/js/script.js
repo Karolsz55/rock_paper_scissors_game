6ea085c63b79989f77cea6654009bfa0e36ccc8a
Karolsz55 <karolsz55@gmail.com>
2023-01-15 20:58:42 +0000

    Add highlight to user & computer choices in game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 /*
-This code sets variables for the
-scoreboard.
+Global variables.
 */
 let userScore = 0;
 let computerScore = 0;
 let userScoreValue = document.getElementById("user-score-value");
 let computerScoreValue = document.getElementById("computer-score-value");
+let userChoiceBtn = null;
+let computerChoiceBtn = null;
 
 /*
 This code executes functionality
@@ -18,7 +19,7 @@ function resetScore() {
     userScoreValue.innerHTML = userScore;
     computerScoreValue.innerHTML = computerScore;
     let result = document.getElementById("result");
-    result.innerHTML = "";
+    result.innerHTML = "Here we go again!";
   }
 
 /*
@@ -30,13 +31,25 @@ function play(userChoice) {
     let choices = ["rock", "paper", "scissors", "lizard", "spock"];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     let result = document.getElementById("result");
+    userChoiceBtn = document.getElementById(userChoice);
+    computerChoiceBtn = document.getElementById(computerChoice);
 
-    if (userScore >= 10 || computerScore >= 10) {
-        alert("Game over!");
+    // Remove highlight class from all buttons
+    let buttons = document.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("highlight");
+    }
+
+    // Add highlight class to current user and computer choices
+    userChoiceBtn.classList.add("highlight");
+    computerChoiceBtn.classList.add("highlight");
+
+
+    if (userScore >= 5 || computerScore >= 5) {
+        alert("Game over! Start again!");
         return;
     }
    
-    
     if (userChoice === computerChoice) {
         result.innerHTML = "It's a tie!";
     } else if (
